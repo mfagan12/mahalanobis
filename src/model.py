b0ba2e77501af503543da52888a5a0b94d727bbf
Michael Fagan <mfagan12@gmail.com>
2022-02-18 17:25:57 -0500

    Working on MNIST/KMNIST example

--- src/model.py
+++ src/model.py
@@ -1,9 +1,22 @@
 import torch
-import pytorch_lighting as pl
+from torch.nn import functional as F
+import torchvision
+import pytorch_lightning as pl
 
 class Classifier(pl.LightningModule):
     def __init__(self):
-        pass
+        super().__init__()
+        self.model = torchvision.models.resnet18(pretrained=True)
+        self.model.fc = torch.nn.Linear(512, 10)
 
-    def forward():
-        pass
+    def forward(self, x):
+        return F.softmax(self.model(x), dim=1)
+    
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.model(x)
+        loss = F.cross_entropy(y_hat, y)
+        return loss
+    
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.001)
