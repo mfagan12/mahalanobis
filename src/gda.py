51ea9ed8f26e2d4b0c0ebdd364d93d37fa8a5e75
Michael Fagan <mfagan12@gmail.com>
2022-02-18 17:25:42 -0500

    Einsum mahalanobis working correctly

--- src/gda.py
+++ src/gda.py
@@ -8,16 +8,17 @@ def mahalanobis(samples, mean, covariance):
     return np.sqrt(np.diag(shifted @ covariance @ shifted.T))
 
 
-def mahalanobis_einsum(samples, means, covariances):
-    # samples: num_examples X dim
-    # means: num_clusters X dim
-    # covariances: num_clusters, dim, dim
+def batch_mahalanobis_einsum(samples, means, covariances, optimize=True):
     num_samples, dim = samples.shape
     num_clusters = len(means)
-    # samples: num_examples X num_clusters X dim
-    samples = np.tile(samples, (num_clusters, 1, 1))
+    samples = np.expand_dims(samples, axis=1)
+    samples = np.tile(samples, (1, num_clusters, 1))
     shifted = samples - means
-    return np.einsum("ijk,lkm,nom -> ij", shifted, covariances, shifted)
+    # Compute Mahalanobis distance of each point from each cluster. The einsum
+    # computes shifted @ covariance @ shifted.T for each cluster efficiently.
+    return np.sqrt(np.einsum("ijk,jkl,ijl -> ij", 
+                             shifted, covariances, shifted, 
+                             optimize=optimize))
 
 
 def make_test_data(n_samples: int = 1000,
@@ -41,16 +42,29 @@ def train_mahalanobis(data,
     return distances
 
 
-def inference_mahalanobis(data,
+def naive_batch_mahalanobis(data,
                           means,
                           covariances):
+    #! Use the more efficient mahalanobis_einsum
     distances = [mahalanobis(data, mean, covariance) 
                  for mean, covariance 
                  in zip(means, covariances)]
     return np.array(distances).T
 
 
-def gda(data, labels):
+def gda_train(data, labels, threshold):
+    means, covariances = fit_qda(data, labels)
+    distances = train_mahalanobis(data, labels, means, covariances)
+    cutoff_dist = distance_at_threshold(distances, threshold)
+    return means, covariances, cutoff_dist
+
+
+def gda_inference(data, means, covariances, cutoff_dist):
+    distances = batch_mahalanobis_einsum(data, means, covariances)
+    return (distances <= cutoff_dist).any(axis=1)
+
+
+def fit_qda(data, labels):
     qda = QuadraticDiscriminantAnalysis(store_covariance=True)
     qda.fit(data, labels)
     means = qda.means_
@@ -60,5 +74,5 @@ def gda(data, labels):
 
 def distance_at_threshold(distances, threshold):
     n = len(distances)
-    sorted_dists = sorted(distances)
+    sorted_dists = sorted(list(distances))
     return sorted_dists[int(n * threshold)]
